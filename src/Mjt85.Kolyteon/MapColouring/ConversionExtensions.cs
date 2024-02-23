f2b9204d311bdfe8934bb03759323e2885174bce
Matt Tantony <matt.tantony@gmail.com>
2024-02-23 10:24:23 +0000

    feat(map-colouring): add solution conversion
    Added a new extension method that can convert an enumerable of variable/
    domain value assignments (i.e. obtained by a binary CSP solving
    algorithm) into a dictionary of Region/Colour key-value pairs. The
    result can then be validated by a MapColouringPuzzle instance.

---
+++ src/Mjt85.Kolyteon/MapColouring/ConversionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Mjt85.Kolyteon.Modelling;
+
+namespace Mjt85.Kolyteon.MapColouring;
+
+/// <summary>
+///     Contains a single conversion extension method.
+/// </summary>
+public static class ConversionExtensions
+{
+    /// <summary>
+    ///     Converts this enumerable into a Map Colouring puzzle solution dictionary.
+    /// </summary>
+    /// <remarks>
+    ///     The returned dictionary can be used as the argument to a <see cref="MapColouringPuzzle.ValidSolution" />
+    ///     method invocation on a <see cref="MapColouringPuzzle" /> instance.
+    /// </remarks>
+    /// <param name="assignments">The assignments to be converted.</param>
+    /// <returns>
+    ///     A new <see cref="Dictionary{TKey,TValue}" /> instance containing a key-value pair for every item in this
+    ///     enumerable.
+    /// </returns>
+    public static IReadOnlyDictionary<Region, Colour> ToPuzzleSolution(this IEnumerable<Assignment<Region, Colour>> assignments)
+    {
+        return assignments.ToDictionary(a => a.Variable, a => a.DomainValue);
+    }
+}
---
+++ tests/Mjt85.Kolyteon.UnitTests/MapColouring/ConversionExtensionsTests.cs
@@ -0,0 +1,54 @@
+﻿using Mjt85.Kolyteon.MapColouring;
+using Mjt85.Kolyteon.Modelling;
+
+namespace Mjt85.Kolyteon.UnitTests.MapColouring;
+
+/// <summary>
+///     Unit tests for the <see cref="ConversionExtensions" /> extension method class.
+/// </summary>
+public sealed class ConversionExtensionsTests
+{
+    [UnitTest]
+    public sealed class ToPuzzleSolution_Method
+    {
+        [Fact]
+        public void InstanceIsNonEmptyEnumerable_ReturnsDictionaryOfKeyValuePairs()
+        {
+            // Arrange
+            Region R0 = Region.FromId("R0");
+            Region R1 = Region.FromId("R1");
+            Region R2 = Region.FromId("R2");
+
+            IEnumerable<Assignment<Region, Colour>> sut =
+            [
+                new Assignment<Region, Colour>(R0, Colour.Cyan),
+                new Assignment<Region, Colour>(R1, Colour.Magenta),
+                new Assignment<Region, Colour>(R2, Colour.Yellow)
+            ];
+
+            // Act
+            IReadOnlyDictionary<Region, Colour> result = sut.ToPuzzleSolution();
+
+            // Assert
+            result.Should().BeEquivalentTo(new Dictionary<Region, Colour>
+            {
+                [R0] = Colour.Cyan,
+                [R1] = Colour.Magenta,
+                [R2] = Colour.Yellow
+            }, options => options.WithoutStrictOrdering());
+        }
+
+        [Fact]
+        public void InstanceIsEmptyEnumerable_ReturnsEmptyDictionary()
+        {
+            // Arrange
+            IEnumerable<Assignment<Region, Colour>> sut = Array.Empty<Assignment<Region, Colour>>();
+
+            // Act
+            IReadOnlyDictionary<Region, Colour> result = sut.ToPuzzleSolution();
+
+            // Assert
+            result.Should().BeEmpty();
+        }
+    }
+}
