49c72a748c6b49c957a1ea408986123c8d168f3f
Matt Tantony <matt.tantony@gmail.com>
2024-02-23 10:34:42 +0000

    feat(n-queens): add solution conversion
    Added a new extension method that can convert an enumerable of variable/
    domain value assignments (i.e. obtained by a binary CSP solving
    algorithm) into a list of Queen instances. The result can then be
    validated by an NQueensPuzzle instance.

---
+++ src/Mjt85.Kolyteon/NQueens/ConversionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Mjt85.Kolyteon.Modelling;
+
+namespace Mjt85.Kolyteon.NQueens;
+
+/// <summary>
+///     Contains a single conversion extension method.
+/// </summary>
+public static class ConversionExtensions
+{
+    /// <summary>
+    ///     Converts this enumerable into an <i>N</i>-Queens puzzle solution list.
+    /// </summary>
+    /// <remarks>
+    ///     The returned list can be used as the argument to an <see cref="NQueensPuzzle.ValidSolution" /> method invocation on
+    ///     a <see cref="NQueensPuzzle" /> instance.
+    /// </remarks>
+    /// <param name="assignments">The assignments to be converted.</param>
+    /// <returns>
+    ///     A new read-only list of <see cref="Queen" /> instances, containing one instance for every assignment in this
+    ///     enumerable.
+    /// </returns>
+    public static IReadOnlyList<Queen> ToPuzzleSolution(this IEnumerable<Assignment<int, Queen>> assignments)
+    {
+        return assignments.Select(a => a.DomainValue).ToArray();
+    }
+}
---
+++ tests/Mjt85.Kolyteon.UnitTests/NQueens/ConversionExtensionsTests.cs
@@ -0,0 +1,51 @@
+﻿using Mjt85.Kolyteon.Modelling;
+using Mjt85.Kolyteon.NQueens;
+
+namespace Mjt85.Kolyteon.UnitTests.NQueens;
+
+/// <summary>
+///     Unit tests for the <see cref="ConversionExtensions" /> extension method class.
+/// </summary>
+public sealed class ConversionExtensionsTests
+{
+    [UnitTest]
+    public sealed class ToPuzzleSolution_Method
+    {
+        [Fact]
+        public void InstanceIsNonEmptyEnumerable_ReturnsListOfDomainValues()
+        {
+            // Arrange
+            Queen Col0Row2 = new(0, 2);
+            Queen Col1Row0 = new(1, 0);
+            Queen Col2Row3 = new(2, 3);
+            Queen Col3Row1 = new(3, 1);
+
+            IEnumerable<Assignment<int, Queen>> sut =
+            [
+                new Assignment<int, Queen>(0, Col0Row2),
+                new Assignment<int, Queen>(1, Col1Row0),
+                new Assignment<int, Queen>(2, Col2Row3),
+                new Assignment<int, Queen>(3, Col3Row1)
+            ];
+
+            // Act
+            IReadOnlyList<Queen> result = sut.ToPuzzleSolution();
+
+            // Assert
+            result.Should().Equal(Col0Row2, Col1Row0, Col2Row3, Col3Row1);
+        }
+
+        [Fact]
+        public void InstanceIsEmptyEnumerable_ReturnsEmptyList()
+        {
+            // Arrange
+            IEnumerable<Assignment<int, Queen>> sut = Array.Empty<Assignment<int, Queen>>();
+
+            // Act
+            IReadOnlyList<Queen> result = sut.ToPuzzleSolution();
+
+            // Assert
+            result.Should().BeEmpty();
+        }
+    }
+}
